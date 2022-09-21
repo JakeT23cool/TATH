88f19f1b9a5ab38667a1b915e3038bc28e028cd4
JakeT23 <69044307+JakeT23cool@users.noreply.github.com>
2022-09-21 22:41:36 +0100

    hope this works lol

--- answer.py
+++ answer.py
@@ -13,21 +13,25 @@ def urltemplate(level, topic="", quiz=""):
         else: return baseurl+"/"+level+"/"+topic+"/"+quiz
 
 
+print("Welcome to JakeT23's TAT bot - For Educational Purposes only.")
 if exists("answers.json") != True:
     urllib.request.urlretrieve("https://raw.githubusercontent.com/JakeT23cool/TATH/stablebranch/answers.json", "answers.json")
     print("Notice - remember to set your session cookies")
-#    print("Notice - When using the 'a' tag please input the first test in that topic. - ALWAYS USE A FLAG")
-#    print("Notice - use the 's' flag for a single test use the 'a' tag for all of that topic. for example 'a https://www.testandtrack.io/index.php/studenttest/test/2/9/123'")
+    print("Notice - When using the 'a' tag please input the first test in that topic. - ALWAYS USE A FLAG")
+    print("Notice - use the 's' flag for a single test use the 'a' tag for all of that topic. for example 'a https://www.testandtrack.io/index.php/studenttest/test/2/9/123'")
+else:
+    print("Notice - Always use a flag")
+#url = input("url: ")
 
-url = input("url: ")
-#data = input(": ")
-#urlnflag  = url.split(" ")
-#flag = urlnflag[0].lower()
-#url = urlnflag[1]
-#if flag not in ["a", "s"]:
-#    print("ERROR > Please specify a flag ")
-#    print(">> use the 's' flag for a single test use the 'a' tag for all of that topic. for example 'a https://www.testandtrack.io/index.php/studenttest/test/2/9/123")
-#    quit()
+session_cookies = input("SESSION COOKIES: ")
+data = input(": ")
+urlnflag  = data.split(" ")
+flag = urlnflag[0].lower()
+url = urlnflag[1]
+if flag not in ["a", "s"]:
+    print("ERROR > Please specify a flag ")
+    print(">> use the 's' flag for a single test use the 'a' tag for all of that topic. for example 'a https://www.testandtrack.io/index.php/studenttest/test/2/9/123")
+    quit()
 split_by_s = url.split("/")[::-1]
 QuizID  = split_by_s[0]
 TopicID = split_by_s[1]
@@ -46,44 +50,61 @@ HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
         "Sec-Fetch-Mode": "navigate",
         "Sec-Fetch-Site": "same-origin",
         "Sec-Fetch-User": "?1",
-        "Cookie": cookies,
+        "Cookie": session_cookies,
         "Referer": "https://www.testandtrack.io/index.php/profile"}
 
+def TakeTest(HEADERS, LevelID, TopicID, QuizID):
+    r = requests.post('https://www.testandtrack.io/index.php/profile/givetest', headers=HEADERS, data={
+                "submit": "Start+Test",
+                "hdnpassreq": "N",
+                "hdnispremium": "0",
+                "hdnSelectedLevel": "",
+                "hdnSelectedTopic": "",
+                "hdnSelectedQuiz": "",
+                "hdnSelectedLevelid": LevelID,
+                "hdnSelectedTopicid": TopicID,
+                "hdnSelectedQuizid": QuizID,
+                "hdnquizurl": urltemplate(LevelID, TopicID, QuizID),
+                "hdnquizurl-topic": urltemplate(LevelID, TopicID),
+                "hdnquizurllevel":	urltemplate(LevelID),
+                "ddllevel":	LevelID,
+                "ddlTopic":	TopicID,
+                "ddlQuiz":	QuizID,
+                "hdnNextQuiz":	str(int(QuizID)+1),
+    })
 
-r = requests.post('https://www.testandtrack.io/index.php/profile/givetest', headers=HEADERS, data={
-            "submit": "Start+Test",
-            "hdnpassreq": "N",
-            "hdnispremium": "0",
-            "hdnSelectedLevel": "",
-            "hdnSelectedTopic": "",
-            "hdnSelectedQuiz": "",
-            "hdnSelectedLevelid": LevelID,
-            "hdnSelectedTopicid": TopicID,
-            "hdnSelectedQuizid": QuizID,
-            "hdnquizurl": urltemplate(LevelID, TopicID, QuizID),
-            "hdnquizurl-topic": urltemplate(LevelID, TopicID),
-            "hdnquizurllevel":	urltemplate(LevelID),
-            "ddllevel":	LevelID,
-            "ddlTopic":	TopicID,
-            "ddlQuiz":	QuizID,
-            "hdnNextQuiz":	str(int(QuizID)+1),
-})
+    answers = json.loads(open("answers.json").read())
+    soup = BeautifulSoup(r.text, "html.parser")
 
-answers = json.loads(open("answers.json").read())
-soup = BeautifulSoup(r.text, "html.parser")
 
-items = ["quizID", "total_ques", "total_marks", "total_time", "testID", "hdnTopic", "hdnLevel", "hdnNextQuiz","hdnSelectedLevel", "hdnSelectedTopic", "hdnSelectedQuiz", "hdnuseremail"]
-values = []
-for i in items:
-    values.append(soup.findAll("input", {"name":i})[0].get('value'))
+    items = ["quizID", "total_ques", "total_marks", "total_time", "testID", "hdnTopic", "hdnLevel", "hdnNextQuiz","hdnSelectedLevel", "hdnSelectedTopic", "hdnSelectedQuiz", "hdnuseremail"]
+    values = []
+    for i in items:
+        values.append(soup.findAll("input", {"name":i})[0].get('value'))
 
-datas = dict(zip(items, values))
+    datas = dict(zip(items, values))
 
-for i in answers[quiz_indentifier].keys():
-    datas[i] = answers[quiz_indentifier][i][0]
+    for i in answers[quiz_indentifier].keys():
+        datas[i] = answers[quiz_indentifier][i][0]
 
-timett = random.uniform(7.225, 25.231)
+    timett = random.uniform(7.225, 25.231)
 
-print("Inputting answers, This will take in excess of", timett, "seconds")
-time.sleep(timett)
-requests.post('https://www.testandtrack.io/index.php/profile/giveTestDemo/'+datas["quizID"], headers=HEADERS, data=datas)
+    print("Inputting answers, This will take in excess of", timett, "seconds")
+    time.sleep(timett)
+    requests.post('https://www.testandtrack.io/index.php/profile/giveTestDemo/'+datas["quizID"], headers=HEADERS, data=datas)
+    requests.post('https://www.testandtrack.io/index.php/profile/keepsessionalive', headers=HEADERS)
+if flag == "a":
+    topicjson = json.loads(requests.post("https://www.testandtrack.io/index.php/profile/getQuizByTopic/"+TopicID, headers=HEADERS).text) # Safeguard
+    ids = []
+    for xy in topicjson:
+        if int(xy["display_questions"]) > 0:
+            ids.append(xy["id"])
+    for i in ids:
+        QuizID = i
+        try:
+            TakeTest(HEADERS, LevelID, TopicID, QuizID)
+        except Exception as e:
+            print("Probably finished some tests, unfortunately there was a bug: ", e)
+        currurl = urltemplate(LevelID, TopicID, QuizID)
+elif flag == "s":
+    TakeTest(HEADERS, LevelID, TopicID, QuizID)
